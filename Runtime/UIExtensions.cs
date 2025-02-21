4041098bd0ca0d349156f4442ccc24dcd1489ad6
Jean-Denis Boivin <jd@boiv.in>
2025-02-20 22:08:19 -0500

    UI Extensions

---
+++ Runtime/UIExtensions.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace JD.Extensions
+{
+    public static class UIExtensions
+    {
+        public static Image SetAlpha(this Image image, float value)
+        {
+            image.color = image.color.ToAlpha(value);
+            return image;
+        }
+    }
+}
