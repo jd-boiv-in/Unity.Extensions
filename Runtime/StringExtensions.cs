d312c1ec0148a256aa2e15f6b514857ed50cf5cd
Jean-Denis "JD" Boivin <jd@boiv.in>
2025-03-28 12:25:12 -0400

    String extensions

---
+++ Runtime/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace JD.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ToCapitalize(this string str)
+        {
+            return $"{str.Substring(0, 1).ToUpperInvariant()}{str.Substring(1)}";
+        }
+    }
+}
