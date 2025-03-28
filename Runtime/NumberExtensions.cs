09430ca459f86357165b00671fbb57a2d3ab268f
Jean-Denis "JD" Boivin <jd@boiv.in>
2025-03-28 13:22:21 -0400

    Modulo support

--- Runtime/NumberExtensions.cs
+++ Runtime/NumberExtensions.cs
@@ -4,15 +4,31 @@ namespace JD.Extensions
 {
     public static class NumberExtensions
     {
+        /// Inclusive random
         public static float Random(this float value)
         {
-            return UnityEngine.Random.Range(0, value); // Already Inclusive
+            return UnityEngine.Random.Range(0, value);
         }
         
+        /// Inclusive random
         public static int Random(this int value)
         {
             if (value == 0) return 0;
-            return UnityEngine.Random.Range(0, value + 1); // Inclusive
+            return UnityEngine.Random.Range(0, value + 1);
+        }
+
+        /// Modulo that wraps around negative value so (-2 % 12) is 10 not -2 / 2
+        public static int Mod(this int value, int mod)
+        {
+            if (value < 0)
+            {
+                var m = value % mod;
+                if (m == 0) return 0;
+                
+                return mod + (value % mod);
+            }
+            
+            return value % mod;
         }
     }
 }
