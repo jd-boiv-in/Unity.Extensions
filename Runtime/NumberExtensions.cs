5f331b98222fb3d237d51a25743d2eab640e7508
Jean-Denis "JD" Boivin <jd@boiv.in>
2025-03-28 10:52:04 -0400

    Random on number as well

---
+++ Runtime/NumberExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace JD.Extensions
+{
+    public static class NumberExtensions
+    {
+        public static float Random(this float value)
+        {
+            return UnityEngine.Random.Range(0, value); // Already Inclusive
+        }
+        
+        public static int Random(this int value)
+        {
+            if (value == 0) return 0;
+            return UnityEngine.Random.Range(0, value + 1); // Inclusive
+        }
+    }
+}
