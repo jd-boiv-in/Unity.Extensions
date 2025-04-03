3a6b23414c374ab702cdd012df988028c72ee131
Jean-Denis "JD" Boivin <jd@boiv.in>
2025-04-03 15:50:51 -0400

    string + gameobject extension

---
+++ Runtime/GameObjectExtensions.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace JD.Extensions
+{
+    public static class GameObjectExtensions
+    {
+        public static void RemoveAll(this GameObject obj)
+        {
+            while (obj.transform.childCount > 0)
+                Object.Destroy(obj.transform.GetChild(0));
+        }
+        
+        public static void RemoveAll(this Transform trs)
+        {
+            while (trs.childCount > 0)
+                Object.Destroy(trs.GetChild(0));
+        }
+    }
+}
--- Runtime/StringExtensions.cs
+++ Runtime/StringExtensions.cs
@@ -1,10 +1,17 @@
-﻿namespace JD.Extensions
+﻿using System.Text.RegularExpressions;
+
+namespace JD.Extensions
 {
     public static class StringExtensions
     {
         public static string ToCapitalize(this string str)
         {
             return $"{str.Substring(0, 1).ToUpperInvariant()}{str.Substring(1)}";
+        }
+        
+        public static string ToClean(this string str)
+        {
+            return Regex.Replace(str, @"[^a-zA-Z0-9]", "");
         }
     }
 }
