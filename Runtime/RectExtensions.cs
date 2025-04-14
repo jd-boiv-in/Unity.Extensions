b8896833eb45c30d2c34c10fd1449534d1364f55
Jean-Denis "JD" Boivin <jd@boiv.in>
2025-04-14 18:19:04 -0400

    More rotate rect

--- Runtime/RectExtensions.cs
+++ Runtime/RectExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 namespace JD.Extensions
@@ -9,8 +10,13 @@ namespace JD.Extensions
         public static Vector2 BottomRight(this Rect rect) => new Vector2(rect.xMax, rect.yMin);
         public static Vector2 TopLeft(this Rect rect) => new Vector2(rect.xMin, rect.yMax);
         public static Vector2 TopRight(this Rect rect) => new Vector2(rect.xMax, rect.yMax);
+        
+        public static Vector2Int BottomLeft(this RectInt rect) => new Vector2Int(rect.xMin, rect.yMin);
+        public static Vector2Int BottomRight(this RectInt rect) => new Vector2Int(rect.xMax, rect.yMin);
+        public static Vector2Int TopLeft(this RectInt rect) => new Vector2Int(rect.xMin, rect.yMax);
+        public static Vector2Int TopRight(this RectInt rect) => new Vector2Int(rect.xMax, rect.yMax);
 
-        public static Rect Rotate(this Rect rect, float degrees, Vector2 pivot = default)
+        public static Rect Rotate(this Rect rect, Vector2 pivot, float degrees)
         {
             // Convert angle to radians
             var radAngle = degrees * Mathf.Deg2Rad;
@@ -35,7 +41,12 @@ namespace JD.Extensions
             return Rect.MinMaxRect(min.x, min.y, max.x, max.y);
         }
 
-        public static RectInt Rotate(this RectInt rect, int degrees, Vector2Int pivot = default)
+        public static Rect Rotate(this Rect rect, float degrees)
+        {
+            return Rotate(rect, new Vector2(rect.xMin, rect.yMin), degrees);
+        }
+
+        public static RectInt Rotate(this RectInt rect, Vector2Int pivot, int degrees)
         {
             degrees = ((degrees % 360) + 360) % 360;
             return degrees switch
@@ -48,25 +59,59 @@ namespace JD.Extensions
             };
         }
 
-        public static RectInt Rotate90(this RectInt rect, Vector2Int pivot = default)
+        private static RectInt Rotate90(this RectInt rect, Vector2Int pivot)
         {
             var xMin = pivot.x - (rect.yMax - pivot.y);
             var yMin = pivot.y + (rect.xMin - pivot.x);
-            return new RectInt(xMin, yMin, rect.height, rect.width);
+            return new RectInt(xMin + 1, yMin, rect.height, rect.width);
         }
 
-        public static RectInt Rotate180(this RectInt rect, Vector2Int pivot = default)
+        private static RectInt Rotate180(this RectInt rect, Vector2Int pivot)
         {
             var xMin = pivot.x - (rect.xMax - pivot.x);
             var yMin = pivot.y - (rect.yMax - pivot.y);
-            return new RectInt(xMin, yMin, rect.width, rect.height);
+            return new RectInt(xMin + 1, yMin + 1, rect.width, rect.height);
         }
 
-        public static RectInt Rotate270(this RectInt rect, Vector2Int pivot = default)
+        private static RectInt Rotate270(this RectInt rect, Vector2Int pivot)
         {
             var xMin = pivot.x + (rect.yMin - pivot.y);
             var yMin = pivot.y - (rect.xMax - pivot.x);
-            return new RectInt(xMin, yMin, rect.height, rect.width);
+            return new RectInt(xMin, yMin + 1, rect.height, rect.width);
+        }
+        
+        public static RectInt Rotate(this RectInt rect, int degrees)
+        {
+            var pivot = new Vector2Int(rect.xMin, rect.yMin);
+            degrees = ((degrees % 360) + 360) % 360;
+            return degrees switch
+            {
+                0 => rect,
+                90 => Rotate90(rect, pivot),
+                180 => Rotate180(rect, pivot),
+                270 => Rotate270(rect, pivot),
+                _ => rect
+            };
+        }
+        
+        public static RectInt Sub(this RectInt rect, Vector2Int pt)
+        {
+            return new RectInt(rect.xMin - pt.x, rect.yMin - pt.y, rect.width, rect.height);
+        }
+        
+        public static RectInt Add(this RectInt rect, Vector2Int pt)
+        {
+            return new RectInt(rect.xMin + pt.x, rect.yMin + pt.y, rect.width, rect.height);
+        }
+        
+        public static Rect Sub(this Rect rect, Vector2 pt)
+        {
+            return new Rect(rect.xMin - pt.x, rect.yMin - pt.y, rect.width, rect.height);
+        }
+        
+        public static Rect Add(this Rect rect, Vector2 pt)
+        {
+            return new Rect(rect.xMin + pt.x, rect.yMin + pt.y, rect.width, rect.height);
         }
     }
 }
