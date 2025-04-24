46c2c6775781249639a9f26bcd1bc8c9c8d5382a
Jean-Denis "JD" Boivin <jd@boiv.in>
2025-04-24 13:48:13 -0400

    Materials Extensions

---
+++ Runtime/MaterialExtensions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace JD.Extensions
+{
+    public static class MaterialExtensions
+    {
+        public static void SaveFloat(this Material material, int property, float value)
+        {
+            material.SetFloat(property, value);
+            
+#if UNITY_EDITOR
+            EditorUtility.SetDirty(material);
+            AssetDatabase.SaveAssetIfDirty(material);
+#endif
+        }
+    }
+}
