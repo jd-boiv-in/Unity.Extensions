60be4221d0b319f3c366e6be789afc23be9c80c4
Jean-Denis "JD" Boivin <jd@boiv.in>
2025-05-04 20:24:20 -0400

    Fast digit extract

--- Runtime/NumberExtensions.cs
+++ Runtime/NumberExtensions.cs
@@ -5,6 +5,52 @@ namespace JD.Extensions
 {
     public static class NumberExtensions
     {
+        /// Return the number of digits (base 10 obviously)
+        public static int Digits(this int n)
+        {
+            if (n >= 0)
+            {
+                if (n < 10) return 1;
+                if (n < 100) return 2;
+                if (n < 1000) return 3;
+                if (n < 10000) return 4;
+                if (n < 100000) return 5;
+                if (n < 1000000) return 6;
+                if (n < 10000000) return 7;
+                if (n < 100000000) return 8;
+                if (n < 1000000000) return 9;
+                return 10;
+            }
+            else
+            {
+                if (n > -10) return 2;
+                if (n > -100) return 3;
+                if (n > -1000) return 4;
+                if (n > -10000) return 5;
+                if (n > -100000) return 6;
+                if (n > -1000000) return 7;
+                if (n > -10000000) return 8;
+                if (n > -100000000) return 9;
+                if (n > -1000000000) return 10;
+                return 11;
+            }
+        }
+
+        public static int NthDigit(this int n, int digit)
+        {
+            // (_value / (int) Mathf.Pow(10, _digits - i - 1)) % 10;
+
+            if (digit == 0) return n % 10;
+            if (digit == 1) return (n / 10) % 10;
+            if (digit == 2) return (n / 100) % 10;
+            if (digit == 3) return (n / 1000) % 10;
+            if (digit == 4) return (n / 10000) % 10;
+            if (digit == 5) return (n / 100000) % 10;
+            if (digit == 6) return (n / 1000000) % 10;
+            if (digit == 7) return (n / 10000000) % 10;
+            return n % 10;
+        }
+        
         /// Find the next nearest Power of Two
         public static int NextPowerOfTwo(this int x)
         {
